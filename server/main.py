ed389be37427c73764de56bd89fd5b5258019a52
showay <df405102@gmail.com>
2019-05-02 20:49:49 +0800

    Camera Event Added

--- server/main.py
+++ server/main.py
@@ -1,19 +1,40 @@
+from picamera import PiCamera
+import time
+
 from flask import Flask
 import requests
 import Keys
 
 app = Flask(__name__)
 
+camera = PiCamera(resolution=(512, 384))
+
+@app.route('/')
+def index():
+	return "<h1>HSP</HSP>"
+
 @app.route('/<int:id>')
-def index(id):
-	if id == 0: text='HSP已啟動'
-	elif id == 1: text='HSP警報<br>偵測到大門打開，有未授權進入'
-	elif id == 2: text='HSP警報已解除'
-	else: return 'Hi'
+def AlarmSystem(id):
+	time_detected = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+	if id == 0:
+		text='HSP已啟動'
+	elif id == 1:
+		text='HSP警報<br>偵測到大門打開，有未授權進入'
+	elif id == 2:
+		text='HSP警報已解除'
+	else:
+		return 'Hi'
 	data = {'value1': '<br>'+text} #, 'value2': 'http://youtube.com'}
 	r = requests.get("https://maker.ifttt.com/trigger/%s/with/key/%s" % (Keys.EventName, Keys.Key), data = data)
+
+	if id == 1:
+		for i in range(20):
+			pic_name = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+			pic_name += '_%d.jpg' % i
+			camera.capture(pic_name)
+
 	return r.text
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5566)
+	app.run(host='0.0.0.0', port=8008)
